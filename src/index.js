4728f42f4d594b4101b6ec6f16a3ea886cb9222c
akdombrowski <akdflyinvee73@gmail.com>
2021-03-14 07:53:52 -0500

    babel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-function printMsg () {
+export function printMsg () {
   console.log("This is a message from the demo package");
   console.log(jwtDecode());
 }
 
-function jwtDecode() {
+export default function jwtDecode() {
     console.log("jwtDecode");
 }
-
-module.exports = {printMsg, jwtDecode};
