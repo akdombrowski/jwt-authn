43c35237e55d825d3652bdd4fa01d16e97af3d09
akdombrowski <akdflyinvee73@gmail.com>
2021-03-14 09:34:59 -0500

    mocha test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-export function printMsg () {
-  console.log("This is a message from the demo package");
-  console.log(jwtDecode());
+export function printMsg() {
+  console.log('This is a message from the demo package');
 }
 
 export default function jwtDecode() {
-    console.log("jwtDecode");
+  console.log('jwtDecode');
 }
