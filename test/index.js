9040ac7b751eda856c86a8b838c36c1ba2ece61f
akdombrowski <akdflyinvee73@gmail.com>
2021-04-18 08:05:27 -0500

    added some tests for signng and verifying and reverse engineering by decoding then re-encoding or vice versa

--- test/index.js
+++ test/index.js
@@ -5,6 +5,8 @@ import {
   rs256PEMVerify,
   rs256JWKVerify,
   hs256Sign,
+  rs256PEMSign,
+  rs256JWKSign,
   createHeaderPayload,
 } from "../src/index.js";
 import crypto from "crypto";
@@ -15,8 +17,8 @@ chai.config.includeStack = true;
 describe("JWT decoding", () => {
   describe("#jwtDecode()", () => {
     // specification for decoding
-    context("when alg is HS256", function () {
-      context("when using JSON.stringify() on output", function () {
+    context("when when alg is HS256", () => {
+      context("when using JSON.stringify() on output",  () => {
         it("decodes from encoded jwt", () => {
           const jwt =
             "eyJhbGciOiJIUzI1NiIsImN0eSI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaWF0IjoxNjAzMzc2MDExfQ.ixWwz6G_3K0y57BHRYpEh6yxMjxdekYgRQ2sOPCBF-Q";
@@ -31,7 +33,7 @@ describe("JWT decoding", () => {
       });
 
       // specification for decoding
-      context("when using JSON.stringify() on output", function () {
+      context("when using JSON.stringify() on output", () => {
         it("decodes from encoded jwt", () => {
           const jwt =
             "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk";
@@ -50,8 +52,8 @@ describe("JWT decoding", () => {
       });
 
       // specification for decoding
-      context("when using plain output", function () {
-        context("checks deep equal on json object output", function () {
+      context("when using plain output", () => {
+        context("checks deep equal on json object output", () => {
           it("decodes from encoded jwt.", () => {
             const jwt =
               "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk";
@@ -71,10 +73,10 @@ describe("JWT decoding", () => {
       });
     });
 
-    context("alg is rs256.", function () {
+    context("when alg is rs256.", () => {
       // specification for decoding
-      context("when using plain output", function () {
-        context("checks deep equal on json object output", function () {
+      context("when using plain output", () => {
+        context("when using deep equal on json object output", () => {
           it("decodes from encoded jwt.", () => {
             const jwt =
               "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.cC4hiUPoj9Eetdgtv3hF80EGrhuB__dzERat0XF9g2VtQgr9PJbu3XOiZj5RZmh7AAuHIm4Bh-0Qc_lF5YKt_O8W2Fp5jujGbds9uJdbF9CUAr7t1dnZcAcQjbKBYNX4BAynRFdiuB--f_nZLgrnbyTyWzO75vRK5h6xBArLIARNPvkSjtQBMHlb1L07Qe7K0GarZRmB_eSN9383LcOLn6_dO--xi12jzDwusC-eOkHWEsqtFZESc6BfI7noOPqvhJ1phCnvWh6IeYI2w9QOYEUipUTI8np6LbgGY9Fs98rqVt5AXLIhWkWywlVmtVrBp0igcN_IoypGlUPQGe77Rw";
@@ -119,9 +121,9 @@ describe("JWT decoding", () => {
 
 describe("JWT encoding", () => {
   describe("#jwtEncode()", () => {
-    context("alg is HS256", () => {
+    context("when alg is HS256", () => {
       // specification for jwt encoding
-      context("when strict string inputs are used", () => {
+      context("when string literals are used", () => {
         it("encodes a jwt.", () => {
           const decodedHeader = '{"typ":"JWT",\r\n "alg":"HS256"}';
           const decodedPayload =
@@ -158,9 +160,9 @@ describe("JWT encoding", () => {
       });
     });
     // specification for jwt encoding
-    context("alg is RS256", () => {
+    context("when alg is RS256", () => {
       context("when string literals are used", () => {
-        context("and jwk is strting literal", () => {
+        context("and jwk is string literal", () => {
           it("encodes a jwt.", () => {
             const decodedHeader = '{"alg":"RS256"}';
             const decodedPayload =
@@ -186,7 +188,7 @@ describe("JWT encoding", () => {
       context("when JSON objects are used", () => {
         context("and key is pem string", () => {
           it("encodes a jwt. and verifies with public key", () => {
-            const decodedHeader = '{"alg":"RS256"}';
+            const decodedHeader = { alg: "RS256" };
             const decodedPayload = {
               iss: "joe",
               exp: 1300819380,
@@ -375,33 +377,183 @@ JsiBGcDuqIAroTwjs457N4UCAwEAAQ==\n-----END PUBLIC KEY-----"
   });
 });
 
-describe("JWT encoding then decoding", () => {
-  context("when deep equal used", () => {
-    it("they are equal when object input is used", () => {
-      const decodedHeader = { typ: "JWT", alg: "HS256" };
-      const decodedPayload = {
-        iss: "joe",
-        exp: 1300819380,
-        "http://example.com/is_root": true,
-      };
-      const passphrase =
-        "AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow";
+describe("#jwtEncode() and #jwtDecode()", () => {
+  describe("JWT encoding then decoding", () => {
+    context("when alg is HS256", () => {
+      context("when deep equal used", () => {
+        it("the decoding is equal when encoding then re-decoding", () => {
+          const decodedHeader = { typ: "JWT", alg: "HS256" };
+          const decodedPayload = {
+            iss: "joe",
+            exp: 1300819380,
+            "http://example.com/is_root": true,
+          };
+          const passphrase =
+            "AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow";
 
-      const expectedJWT =
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLCJleHAiOjEzMDA4MTkzODAsImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.lliDzOlRAdGUCfCHCPx_uisb6ZfZ1LRQa0OJLeYTTpY";
-      const encoded = jwtEncode(decodedHeader, decodedPayload, passphrase);
+          const expectedJWT =
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLCJleHAiOjEzMDA4MTkzODAsImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.lliDzOlRAdGUCfCHCPx_uisb6ZfZ1LRQa0OJLeYTTpY";
+          const encoded = jwtEncode(decodedHeader, decodedPayload, passphrase);
 
-      expect(encoded).to.equal(expectedJWT);
+          expect(encoded).to.equal(expectedJWT);
 
-      const headerPayload = createHeaderPayload(decodedHeader, decodedPayload);
-      const sig = hs256Sign(headerPayload, passphrase);
-      const objOutput = {
-        header: decodedHeader,
-        payload: decodedPayload,
-        signature: sig,
-      };
-      const decoded = jwtDecode(encoded);
-      expect(decoded).to.deep.equal(objOutput);
+          const headerPayload = createHeaderPayload(
+            decodedHeader,
+            decodedPayload
+          );
+          const sig = hs256Sign(headerPayload, passphrase);
+          const objOutput = {
+            header: decodedHeader,
+            payload: decodedPayload,
+            signature: sig,
+          };
+          const decoded = jwtDecode(encoded);
+          expect(decoded).to.deep.equal(objOutput);
+        });
+      });
+    });
+  });
+
+  describe("JWT decoding then encoding", () => {
+    context("when alg is HS256", () => {
+      context("when deep equal used", () => {
+        it("the encoding is equal when decoding then re-encoding", () => {
+          const decodedHeader = { typ: "JWT", alg: "HS256" };
+          const decodedPayload = {
+            iss: "joe",
+            exp: 1300819380,
+            "http://example.com/is_root": true,
+          };
+          const passphrase =
+            "AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow";
+
+          const encodedJWT =
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLCJleHAiOjEzMDA4MTkzODAsImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.lliDzOlRAdGUCfCHCPx_uisb6ZfZ1LRQa0OJLeYTTpY";
+          const decoded = jwtDecode(encodedJWT);
+
+          const headerPayload = createHeaderPayload(
+            decodedHeader,
+            decodedPayload
+          );
+          const sig = hs256Sign(headerPayload, passphrase);
+          const objOutput = {
+            header: decodedHeader,
+            payload: decodedPayload,
+            signature: sig,
+          };
+          expect(decoded).to.deep.equal(objOutput);
+
+          const encoded = jwtEncode(
+            objOutput.header,
+            objOutput.payload,
+            passphrase
+          );
+
+          expect(encoded).to.equal(encodedJWT);
+        });
+      });
+    });
+  });
+});
+
+describe("Signing and Verification", () => {
+  describe("JWT signing", () => {
+    describe("#hs256Sign()", () => {
+      context("when when alg is hs256", () => {
+        it("correctly generates a signature", () => {
+          const expectedSig = "lliDzOlRAdGUCfCHCPx_uisb6ZfZ1LRQa0OJLeYTTpY";
+          const decodedHeader = { typ: "JWT", alg: "HS256" };
+          const decodedPayload = {
+            iss: "joe",
+            exp: 1300819380,
+            "http://example.com/is_root": true,
+          };
+          const passphrase =
+            "AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow";
+
+          const headerPayload = createHeaderPayload(
+            decodedHeader,
+            decodedPayload
+          );
+
+          const sig = hs256Sign(headerPayload, passphrase);
+          expect(sig).to.equal(expectedSig);
+        });
+      });
+    });
+
+    describe("#rs256Sign()", () => {
+      context("when when alg is rs256", () => {
+        it("correctly generates a signature", () => {
+          const decodedHeader = { alg: "RS256" };
+          const decodedPayload = {
+            iss: "joe",
+            exp: 1300819380,
+            "http://example.com/is_root": true,
+          };
+          const privateKey =
+            "-----BEGIN RSA PRIVATE KEY-----\nMIIJKAIBAAKCAgEAoDEmXwa0fhiB6EA33u8qSIEkR8o26nzrOjLl0xpJ4hfjBMm+\
+            izLb+WudOINw6BmNcHfapLJm1XJxGOqQrbOej1R513z+1GGZH+Ib94RQeQZRdReL\
+            5ZEfZS4H8ONMxAWGfQU/WEaKrp5NgxjHK8wcGwbHBFXZBkc7F0Sumb+IE2kDGJm3\
+            E/I5SGY5WWF+mKvsbGzen290f4tZ29j8yM3RprwKx5TKG/bAf/GDgQFtk+VWv39B\
+O7S3AnR+XhjmEsAsudTAzCeEoW18VOP1EdjLoCzVPUYe6hYuHRT+v2NhZW9srCHp\
+6WtQmh0GTz0d02l1Bbfws6e15lol9t91rlsxr8LxcWIWWzbKgSl8wJ1waR7CYtOW\
+pSo3XGuftu0Fi2aLrsV7wkHyksvf69XYOC9FyxhokfFPgvfYd6zveUAl/Fvl6qYg\
+tbbSfiNrKp3Rvd32hfBy4o7spKNGrTyQorWH8whQlTavSDxzSRcWcNSkZkkAeMlC\
+Jjc2mZTRpps06umVHZxibRiGf40WUMZHX/SzF+ba9fFgTFmfIYvGZ0Kv6AEtJkEz\
+reMjQvmGvt1b8L9FICp7dxcu/CWZE7xBgtYPcDUM9UwCdLBT8ObrLgv5rL/XNImA\
+F8+lUG3k8WPupzOtDQxcAC7J+inb65HDSkK9JsiBGcDuqIAroTwjs457N4UCAwEA\
+AQKCAgB+D++4XFhx2utwUAknwWKvCCH69OFeisu6Uw+Vn1yBNncK75Cpjm6Zy9w/\
++wBBRsqNtWQ0s8hFNPhXuU/9whqHhN0VfWI01FFiS9fiYxfJBfXYhZ0sHZZtU1og\
+cV+Z6xc5D4YX+/trC1zITBmBF/EOO7kmakCNtkSEPSKCD74QOFWdaGowoi/+D6QV\
++Kldj/5QHSTcpyMwtAMVBZ94jq/r3JNHGIosk3RNu0vXm74V4ybvMD+vsrlKj05a\
+0pFMNDt+SjUm8/pl5vku/O2BC/DCrkVpION3J9lGPZ10E8szvgnxNAcWD8OwZCLw\
+/JGaK6VRyLz9RH/bNRonOxcrDMz92y86sTsbjvzU/5e8XdaWZPqmsS0U2i4oxLxU\
+N3GhDvMLkqSNU1DBd+sm0F5hkyjuUEwH79YYOa2T7DyhApMDcKpc6Y4CJP6Oeqev\
+nMGBEuqLRQhf3Iy+Q6YXqYX/ocHnQuxQvkYW5NQVPu+pzSUCmLsKrYWmgVTMEoeZ\
+yt0mFFl4sxw533L3vtjrtoHn+x+ZyoXRBVy5TufIXr7uNBkIFekUSIny+Mg08F1V\
+tgD91B92JihJR6rss51OgMec84P0vDRvFtliQTfQDxDu2S8eJ82wxdz7Y8en0wcT\
+P/zZIU7xEOdt+s67M6OQRIlKexNTC//XYxjqDCKX9ugOnxh4SQKCAQEA0UARkjGv\
+A1mhYFESugOTl6WzksOqQa98kaENFR63ZL2thue7CqPK48y68LYuFIou/vFAzi8G\
+Z2EyQajJSbf6HF4r5HxTLqs4EePzJfM9Nq4bR76VC382o3YJsyYe3WFu6K0JzBKa\
+Bo1Uf5hcV9lkE5xpZxTb90hZVZoAcmDxarYOUMPDBGkU8TIHJCidCQe3Yi2KUlCq\
+kshONigPTtT2V98N9TC3swoE01qb0S4LvCjQUjkuGayCNjjcQ3FGLs6Ni7sMBCeW\
+D2On8izC6kPpPQWyb6QClOgQ95c3a8aQ7hcuSi2NHTpkcl2moWx/2e1/ziA0rdSo\
+pOGcSl1soLgMdwKCAQEAw/s5tUdWhrsFyT8T58I6qURvlEaTWTZstUmU2Q3Uq1iC\
+sMDroNOYxwX2xaMWTDLw0w8EhH7Y1Nt4oscnGKtj7R+J1+UdEv7muAqBnUpLxxAJ\
+nCrr/bsl3T7oIzlob46KiFslJoj/mfZiksMzkQBXISkS7MDFyE7Wob1FeTI9xPYq\
+bJ/C7b2zHDse28z8frAHShjoUxPO3oLDYNwjLtGAhehllZK0QX7huK5Ltr1oN9lR\
+5EdpFgeP85hQ+abXNW4sX0ZmHMb6uvpig9LemP+ya9ZHzJNRdKkyQ94XLFhKEA5b\
+r82fUvq99gC3hXZBd8hifd+fR53PlX7pljaWyiWm4wKCAQBTzLq8t5UFXHHaZt1/\
+DUugYcXmsuGOaL8Ue1EH8kMz3xqNemRXwHzSef0rxWny49wqUWfIquB/qlc8IGJs\
+WXeawMQHPxr5B6zuzUFlI2KScWuPce2yqwGwuObtjgiyVADcwd+4kIKKuTLr3pKs\
+w+zmRa+YjqwA/kJ4m7IuHFDSkZJuGdgR/hta9NQTUQiCw0pX0TEiAhgtrHZWDfkD\
+cxos2rF2TkpjM1H3asQYI76jXhz/sNKbiZCkMmVTxRt8R1A8c24+g7P++cQaon50\
+3zmbLJrnRuGZzM5uuWqCiQUltLbfEhr/Z5Euk/7ViuQw3IJghFlUfGQFlCzJQ1zr\
+iInFAoIBADTYoY8IVYSvGbhSkgOElFzgX1YUUddUnauk08ZTvG0XyOXwn5kWdbP1\
+H73bZD/X3KiM/CLeMYn0RGtgWGKGlMN0Ja9Z0KU2pvAppRnM1vIumOjLiic5N9SZ\
+Ce+1ozuxrBlounPRrVrO3XXFvTn0ZYpDkR7WPH7vA2FJsFol+hIKbxYrXlzNMmcC\
+aifK9qsSvGR2ou8EAFxv1k9gDPyq8YUqTXovvPuMQ0GRfkkgeeHVcAemWefe0ipV\
+gEBRhOabdsuaKB7KaQHSUo5JZkzLZkk91s6LSwOyPCOg147Vn+MEfCTDxZqSknar\
+atFKOg0as7RNgJ3HKBqfxk0DG1YXIRECggEBAMDvDLw5LIblQZq/qJafp4pKeRJI\
+0pIMRcOcqtg8XH2JkiUbBQP/dP5uikzeiLQXCFa+lTAUCNp0c1m0H/KJEsF78Fto\
+yadYGaPQCJEWX3VEjlYECOls+Rajp6SIYD9V9neLLf42MrPDXUtBJkYofZvzcm9T\
+9gKWwNBonHPuEnjvpqFg9VWTNCYeJ/3ZJdvQz7EM5mGvPOnMSXkHQ6WE/oDbfbIN\
+Ky9Bzc5cP6gkQ0zsgzCMOfjVLnHB12c0OrcaAQ1K5qOujZn3LIEc4QiRjNoqOCDv\
+g8W+z36ROKfkVVbmEVHY1Kg9yMo7oKYZEIa5AcAZyxxDoedT0jnlBRaWLtM=\n-----END RSA PRIVATE KEY-----";
+
+          const headerPayload = createHeaderPayload(
+            decodedHeader,
+            decodedPayload
+          );
+          const options = { keyFormat: "pem" };
+
+          const sig = rs256PEMSign(headerPayload, privateKey);
+
+          const expectedSig =
+            "UkNEGPn1I_xQg_ztCjwjzQXetpRMtuLOnGx-6lHB1UH3QE6s8kCkYJLOT1dGMuH_LCcJU41VfGoKTKvAT0ZzDu0Au7g8FL1DFj0HmflpoWsYcuKrSywvqwHL8KSJySYzbSBzNLAYOn1CxfNKRcQyVwRAXJUkZlIL2eE0LRhSwuwQdobHm_oyB4l4OhQSLVgehprGBRLyHxh2fAuyTb3dE2HBXta5YKSPVSbGIvv41KRdqoBoo3a2-GxEMjjS9W7xSjc7Pb7sOt1IJLXeFF5aULI8WsefaW5kPvcdYtGbphxK3qJItudmk_3IuQoMXAZVvLcbfsUu920-gYKKN_iWAb9VDP6WRVOOihTVi5dtFfdzJMfg7rRz2Vprly6fsIDDedERMR-4e6YNE4lmpjjkWx7Roj5xSH0EmfHIiQagwLZ4UYhTpLSYnp_y3Eh7lb7VsoNRBFj_c-26ofB3TLRKqSzJzNdzCJWTZeugYk8Rv75zyh7bGUtX_WmdgwWNKwi4df_nBltB5VZ-UzbQakmqWoZ-HrkkCXrrbooZIySzNBDJJc89rYilIlB3rLv8pu4aSXf8pBp2ii-GZYK4rONK9wPlzYgXa-wMcifUQ7j5bBB9njPITRcp6iT60bAf143vnzGaZ7IE-5GxZ3x8smnBCTqxtRU6cZ3yhXrX5dN9_BM";
+        });
+      });
     });
   });
 });
